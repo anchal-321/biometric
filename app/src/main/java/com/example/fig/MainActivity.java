d0954b059e2ad7e01ef3a5e0f9137d2657511787
Abhishek Agrawal <abhishekagrawal8888@gmail.com>
2020-07-01 23:22:57 +0530

    Fix For Api Under 28

--- app/src/main/java/com/example/fig/MainActivity.java
+++ app/src/main/java/com/example/fig/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.fig;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.biometric.BiometricManager;
-import androidx.biometric.BiometricPrompt;
+
 import androidx.core.content.ContextCompat;
 
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,58 +17,64 @@ import java.util.concurrent.Executor;
 public class MainActivity extends AppCompatActivity {
     TextView tvmesg;
 
+//    BiometricPrompt.PromptInfo promptInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvmesg = findViewById(R.id.tvMessage);
 
-        BiometricManager biometricManager = BiometricManager.from(this);
+        if (Build.VERSION.SDK_INT >= 28) {
+            BiometricManager biometricManager = BiometricManager.from(this);
 
-        switch (biometricManager.canAuthenticate()) {
-            case BiometricManager.BIOMETRIC_SUCCESS:
-                tvmesg.setText("you can use the fingerprint sensor for login");
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
-                tvmesg.setText("the device don't have the fingerprint sensor");
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
-                tvmesg.setText("the biometric sensor is currently unavailable");
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
-                tvmesg.setText("your device dont have any fingerprint saved,please check your security setting");
-                break;
+            switch (biometricManager.canAuthenticate()) {
+                case BiometricManager.BIOMETRIC_SUCCESS:
+                    tvmesg.setText("you can use the fingerprint sensor for login");
+                    break;
+                case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                    tvmesg.setText("the device don't have the fingerprint sensor");
+                    break;
+                case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                    tvmesg.setText("the biometric sensor is currently unavailable");
+                    break;
+                case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                    tvmesg.setText("your device dont have any fingerprint saved,please check your security setting");
+                    break;
 
+            }
+
+            Executor executor = ContextCompat.getMainExecutor(this);
+            BiometricPrompt biometricPrompt = new BiometricPrompt(this, executor, new BiometricPrompt.AuthenticationCallback() {
+                @Override
+                public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+                    Toast.makeText(MainActivity.this, "login success!", Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onAuthenticationFailed() {
+                    super.onAuthenticationFailed();
+                    Toast.makeText(MainActivity.this, "login failed", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("login")
+                    .setSubtitle("use finger to login")
+                    .setNegativeButtonText("cancel")
+                    .build();
+
+            biometricPrompt.authenticate(promptInfo);
         }
-
-        Executor executor = ContextCompat.getMainExecutor(this);
-        BiometricPrompt biometricPrompt = new BiometricPrompt(this, executor, new BiometricPrompt.AuthenticationCallback() {
-            @Override
-            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
-                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-                Toast.makeText(MainActivity.this, "login success!", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
-                Toast.makeText(MainActivity.this, "login failed", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("login")
-                .setSubtitle("use finger to login")
-                .setNegativeButtonText("cancel")
-                .build();
-
-        biometricPrompt.authenticate(promptInfo);
+        else {
+            Toast.makeText(this,"You Dont Have Minimum Requirements For This Type Of Verification",Toast.LENGTH_LONG).show();
+        }
     }
 }
 
